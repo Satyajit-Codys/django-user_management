0623d0771de374dbce97e17707e7f0e2eeb8a5fd
Satyajit@Codys <satyajitdebnath87@gmail.com>
2021-10-11 16:49:23 +0530

    proc to Proc

--- website/settings.py
+++ website/settings.py
@@ -122,11 +122,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
+
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 
 STATIC_URL = '/static/'
-
 
 
 # Default primary key field type
