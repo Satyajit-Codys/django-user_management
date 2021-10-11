6a6ecef463bb87c7bdfc330f27ec9034e57d50c8
Satyajit@Codys <satyajitdebnath87@gmail.com>
2021-10-11 17:17:54 +0530

    url fixing

--- users/urls.py
+++ users/urls.py
@@ -8,12 +8,13 @@ urlpatterns = [
     path("logout", views.logout_request, name= "logout"),
     path("", views.dashboard, name="dashboard"),
     path("users/dashboard.html", views.dashboard, name="dashboard"),
+    
     path('edit/<int:id>', views.edit),  
     path('update/<int:id>', views.update),  
     path('delete/<int:id>', views.destroy),  
 
     url(r"^dashboard", views.dashboard, name="dashboard"),
-    url(r"^login", views.login_request, name="login"),
+    # url(r"^login", views.login_request, name="login"),
     url(r"^logout", views.logout_request, name="logout"),
     url(r"^register", views.register_request, name="register"),
 ]
--- users/views.py
+++ users/views.py
@@ -24,7 +24,7 @@ def login_request(request):
 			username = form.cleaned_data.get('username')
 			password = form.cleaned_data.get('password')
 			user = authenticate(username=username, password=password)
-			print(user)
+			# print(user)
 			if user is not None:
 				login(request, user)
 				messages.info(request, f"You are now logged in as {username}.")
